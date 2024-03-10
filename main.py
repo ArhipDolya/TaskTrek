6b34c30f8cfb5097c2af48612d5e0d98be4e4757
ArhipKokocan <hts1345hjyuhip228888@gmail.com>
2024-03-10 10:40:22 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+import uvicorn
+
+from fastapi import FastAPI
+
+
+app = FastAPI()
+
+
+@app.get('/')
+async def main():
+    return {'message': 'Hello World'}
+
+
+
+if __name__ == '__main__':
+    uvicorn.run(app, host='127.0.0.1', port=8000)
