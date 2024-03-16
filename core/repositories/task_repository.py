0b5785c5523993e53f7cfbd4c568aad429fe1fbe
ArhipKokocan <hts1345hjyuhip228888@gmail.com>
2024-03-16 11:01:28 +0200

    Implement task repository

--- core/repositories/task_repository.py
+++ core/repositories/task_repository.py
@@ -1,13 +1,16 @@
 from abc import ABC, abstractmethod
 from typing import Optional
 
-from core.domain.entities.task import TaskEntity
+from sqlalchemy.orm import Session
+
+from core.entities.task_entity import TaskEntity
+from core.models.task_model import TaskModel
 
 
 class ITaskRepository(ABC):
     
     @abstractmethod
-    async def add(self, task: TaskEntity) -> TaskEntity:
+    async def create(self, task: TaskEntity) -> TaskEntity:
         ...
         
     @abstractmethod
@@ -26,3 +29,29 @@ class ITaskRepository(ABC):
     async def delete(self, task_id: int) -> None:
         ...
 
+
+class SQLTaskRepository(ITaskRepository):
+    
+    def __init__(self, db_session: Session):
+        self.db_session = db_session
+        
+    async def create(self, task: TaskEntity) -> TaskEntity:
+        db_task = TaskModel(**task.dict())
+        print(db_task)
+        self.db_session.add(db_task)
+        self.db_session.commit()
+        self.db_session.refresh(db_task)
+
+        return TaskEntity.model_validate(db_task)
+        
+    async def get_by_id(self, id: int) -> Optional[TaskEntity]:
+        ...
+        
+    async def get_all(self) -> list[TaskEntity]:
+        ...
+        
+    async def update(self, task_id: int, task: TaskEntity) -> TaskEntity:
+        ...
+        
+    async def delete(self, task_id: int) -> None:
+        ...
