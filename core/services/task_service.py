773acf2b38b463d8070956dae212e4f1aa4e4f99
ArhipKokocan <hts1345hjyuhip228888@gmail.com>
2024-03-16 11:02:14 +0200

    Add task service implementation

---
+++ core/services/task_service.py
@@ -0,0 +1,23 @@
+from core.entities.task_entity import TaskEntity
+from core.repositories.task_repository import ITaskRepository
+
+
+class TaskService:
+    
+    def __init__(self, task_repository: ITaskRepository):
+        self.task_repository = task_repository
+        
+    async def create_task(self, task: TaskEntity) -> TaskEntity:
+        return await self.task_repository.create(task)
+    
+    async def get_task_by_id(self, task_id: int) -> TaskEntity:
+        return await self.task_repository.get_by_id(task_id)
+    
+    async def get_all_tasks(self) -> list[TaskEntity]:
+        return await self.task_repository.get_all()
+    
+    async def update_task(self, task_id: int) -> TaskEntity:
+        return await self.task_repository.update(task_id)
+    
+    async def delete(self, task_id: int) -> None:
+        return await self.task_repository.delete(task_id)
