ddfe1af9eedf3282d84b7f75dc9d5afd709cf377
ArhipKokocan <hts1345hjyuhip228888@gmail.com>
2024-03-17 10:18:31 +0200

    Move DI dependencies from handlers layer

---
+++ core/api/v1/dependencies.py
@@ -0,0 +1,19 @@
+from fastapi import APIRouter, Depends
+
+from sqlalchemy.orm import Session
+
+from core.services.task_service import TaskService
+from core.repositories.task_repository import SQLTaskRepository
+from core.app.database import SessionLocal
+
+
+def get_db_session():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+def get_task_service(db_session: Session = Depends(get_db_session)):
+    task_repository = SQLTaskRepository(db_session=db_session)
+    return TaskService(task_repository=task_repository)
--- core/api/v1/handlers/task_handlers.py
+++ core/api/v1/handlers/task_handlers.py
@@ -1,27 +1,14 @@
 from fastapi import APIRouter, Depends
 
-from core.entities.task_entity import TaskEntity
+from core.entities.task_entity import TaskEntity, TaskCreate
 from core.services.task_service import TaskService
-from core.repositories.task_repository import SQLTaskRepository
-from core.app.database import SessionLocal
+from core.api.v1.dependencies import get_task_service
 
 
 router = APIRouter(prefix='/api/v1')
 
 
-def get_db_session():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-        
-    
-def get_task_service(db_session=Depends(get_db_session)):
-    task_repository = SQLTaskRepository(db_session=db_session)
-    return TaskService(task_repository=task_repository)
-
-
 @router.post('/tasks/', response_model=TaskEntity)
-async def create_task(task: TaskEntity, task_service: TaskService = Depends(get_task_service)):
-    return task_service.create_task(task=task)
+async def create_task(task: TaskCreate, task_service: TaskService = Depends(get_task_service)):
+    created_task = await task_service.create_task(task=task)
+    return created_task
